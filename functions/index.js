d765ec96c7d14412ad83f7a4a134f1155856a446
Carlos Azaustre <cazaustre@gmail.com>
2017-09-11 15:46:02 +0200

    add firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const gcs = require('@google-cloud/storage')()
+const vision = require('@google-cloud/vision')()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+exports.isHotdog = functions.storage.object()
+  .onChange(event => {
+    const object = event.data
+
+    if (object.resourceState === 'not_exists' || !object.name) {
+      return console.log('This is a deletion/deploy event.')
+    }
+
+    const file = `gs://${object.bucket}/${object.name}`
+
+    return detectLabels(file)
+  })
+
+function detectLabels (file) {
+  console.log('detecting labels...')
+  return vision.labelDetection({ source: { imageUri: file } })
+    .then(response => {
+      const labels = response[0].labelAnnotations
+
+      let isHotdog = false
+      labels.map(label => {
+        if (label.description === 'hot dog') {
+          isHotdog = true
+        }
+      })
+
+      console.log('the image is hotdog? ', isHotdog)
+      return admin.database().ref('/uploads/photo').update({ isHotdog })
+    })
+}
